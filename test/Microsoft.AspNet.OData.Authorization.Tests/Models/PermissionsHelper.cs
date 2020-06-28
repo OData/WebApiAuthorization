71289fd86e833c4014d8570e5d452b6c57717868
Clément Habinshuti <hello@habbes.xyz>
2020-06-28 23:44:19 +0300

    Add test OData model with permissions

---
+++ test/Microsoft.AspNet.OData.Authorization.Tests/Models/PermissionsHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Vocabularies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.AspNet.OData.Authorization.Tests.Models
+{
+    public class PermissionsHelper
+    {
+        public static void AddPermissionsTo(EdmModel model, IEdmVocabularyAnnotatable target, string restrictionName, params string[] scopes)
+        {
+            model.AddVocabularyAnnotation(new EdmVocabularyAnnotation(
+                target,
+                model.FindTerm(restrictionName),
+                CreatePermission(scopes)));
+        }
+
+        public static IEdmExpression CreatePermission(params string[] scopeNames)
+        {
+            var restriction = new EdmRecordExpression(
+                CreatePermissionProperty(scopeNames));
+
+            return restriction;
+        }
+
+        public static IEdmPropertyConstructor CreatePermissionProperty(params string[] scopeNames)
+        {
+            var scopes = scopeNames.Select(scope => new EdmRecordExpression(
+                   new EdmPropertyConstructor("Scope", new EdmStringConstant(scope)),
+                   new EdmPropertyConstructor("RestrictedProperties", new EdmStringConstant("*"))));
+
+            var permission = new EdmRecordExpression(
+                new EdmPropertyConstructor("SchemeName", new EdmStringConstant("AuthScheme")),
+                new EdmPropertyConstructor("Scopes", new EdmCollectionExpression(scopes)));
+
+            var property = new EdmPropertyConstructor("Permissions", new EdmCollectionExpression(permission));
+            return property;
+        }
+
+
+    }
+}
