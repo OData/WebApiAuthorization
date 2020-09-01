66d061111c97b99e48d274b504f179850a38858e
Clement Habinshuti <clhabins@microsoft.com>
2020-09-01 09:36:33 +0300

    Add sample project for docs

---
+++ samples/ODataAuthorizationDemo/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ODataAuthorizationDemo.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
