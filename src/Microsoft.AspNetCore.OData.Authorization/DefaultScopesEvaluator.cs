b18c0add03496dbc27f8e1125608eccec418ca9c
Clement Habinshuti <clhabins@microsoft.com>
2020-09-07 01:05:37 +0300

    Add comments to DefaultScopesEvaluator

--- src/Microsoft.AspNetCore.OData.Authorization/DefaultScopesEvaluator.cs
+++ src/Microsoft.AspNetCore.OData.Authorization/DefaultScopesEvaluator.cs
@@ -5,6 +5,12 @@ using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.OData.Authorization
 {
+    /// <summary>
+    /// An <see cref="IScopesEvaluator"/> that always returns true.
+    /// It's useful for operations that have no restrictions explicitly
+    /// defined in the model. Such operations are assumed to be always
+    /// allowed regardless of the user's scopes.
+    /// </summary>
     internal class DefaultScopesEvaluator : IScopesEvaluator
     {
         public bool AllowsScopes(IEnumerable<string> scopes)
